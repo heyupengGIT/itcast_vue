04f6d51bcd15a2b359ce04acc50d73eb3d822298
heyupeng <yupeng.he828@aliyun.com>
2018-08-14 15:50:38 +0800

    第三天完成

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -27,6 +27,7 @@
             <el-col :span="4">
               <!-- 显示一级权限的名称 -->
               <el-tag
+                @close="handleClose(scope.row, level1.id)"
                 closable>
                 {{ level1.authName }}
               </el-tag>
@@ -39,6 +40,7 @@
                 <el-col :span="4">
                   <!-- 显示二级权限的名称 -->
                   <el-tag
+                    @close="handleClose(scope.row, level2.id)"
                     type="success"
                     closable>
                     {{ level2.authName }}
@@ -47,6 +49,7 @@
                 <el-col :span="20">
                   <!-- 三级权限 -->
                   <el-tag
+                    @close="handleClose(scope.row, level3.id)"
                     class="level3"
                     v-for="level3 in level2.children"
                     :key="level3.id"
@@ -94,6 +97,7 @@
             plain>
           </el-button>
           <el-button
+            @click="handleOpenSetRightDialog(scope.row)"
             type="success"
             icon="el-icon-check"
             size="mini"
@@ -102,6 +106,25 @@
         </template>
       </el-table-column>
     </el-table>
+
+     <!-- 点击分配权限按钮，弹出对话框 -->
+    <el-dialog
+      title="分配权限"
+      :visible.sync="setRightsDialogVisible">
+      <el-tree
+        ref = 'tree'
+        :data="treeData"
+        :props="defaultProps"
+        node-key= 'id'
+        :default-checked-keys="checkedList"
+        default-expand-all
+        show-checkbox>
+      </el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRightsDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleSetRights">确 定</el-button>
+      </span>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -110,7 +133,16 @@ export default {
   data() {
     return {
       data: [],
-      loading: true
+      loading: true,
+      setRightsDialogVisible : false,
+
+      treeData: [],
+      defaultProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      checkedList: [],
+      currentRoleId: -1
     };
   },
   created() {
@@ -118,6 +150,7 @@ export default {
     this.loadData();
   },
   methods: {
+    // 加载角色列表
     async loadData() {
       const response = await this.$http.get('roles')
 
@@ -130,6 +163,75 @@ export default {
       } else {
         this.$message.error(msg);
       }
+    },
+    // 点击tag的删除按钮，删除当前角色对应的权限
+    async handleClose(role,rightId) {
+      // 发送ajax异步请求
+      // role角色Id   rightId 权限id
+      const response = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+      const { meta: { status, msg } } = response.data
+      // 判断是否成功
+      if (status === 200) {
+        this.$message.success(msg)
+        // 重心加载数据
+        // this.loadData()
+        role.children = response.data.data
+      }else{
+        this.$message.error(error)
+      }
+    },
+
+    // 点击分配权限的按钮，打开分配权限的对话框 
+    async handleOpenSetRightDialog(role) {
+
+      // 记录roleId
+      this.currentRoleId = role.id;
+      
+      this.checkedList = []
+      // role当前角色对象
+      this.setRightsDialogVisible = true
+
+      const response = await this.$http.get('rights/tree')
+
+      this.treeData = response.data.data
+
+      // 把当前角色所拥有的权限id，存储到chekedLIst
+      role.children.forEach((level1) => {
+        level1.children.forEach((level2) => {
+          level2.children.forEach((level3) => {
+            this.checkedList.push(level3.id)
+          })
+        })
+      })
+    },
+    // 点击确定按钮，分配权限
+    async handleSetRights() {
+      // currentRoleId 角色id  -- 点击分配权限按钮记录
+      // rids  权限id的列表
+      // 全选的节点的id (3,5,6,7)
+      const checkedList = this.$refs.tree.getCheckedKeys();
+      // 半选的节点的id
+      const halfCheckedList = this.$refs.tree.getHalfCheckedKeys();
+
+      // const arr = checkedList.concat(halfCheckedList);
+      // console.log(arr);
+      const arr = [...checkedList, ...halfCheckedList];
+
+      // 发送请求
+      const response = await this.$http.post(`roles/${this.currentRoleId}/rights`, {
+        rids: arr.join(',')
+      });
+
+      // 判断是否成功
+      const { meta: { status, msg } } = response.data;
+      if (status === 200) {
+        this.$message.success(msg);
+        this.setRightsDialogVisible = false;
+        // 重新加载数据
+        this.loadData();
+      } else {
+        this.$message.error(msg);
+      }
     }
   }
 }
@@ -138,6 +240,7 @@ export default {
 <style>
 .card {
   height: 100%;
+  overflow: hidden;
 }
 .add-row {
   margin-top: 10px;
